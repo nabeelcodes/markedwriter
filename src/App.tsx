a392b700616ca4c9cf56e448649969c6749ae81d
nabeelcodes <nabeeldesigner91@hotmail.com>
2023-07-06 17:02:05 +0530

    Added fonts for the project
    - `DM MONO` (google-font-CDN): monospace font for textarea editor
      and general purpose use for codeblocks.
    - `MabryPro-Regular/Bold` (self-hosted): sans-serif font for
      rendered markdown and other ui elements.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ export default function App() {
 	return (
 		<>
 			<WindowWidthOverlay />
-			<h1>MARKWRITER</h1>
+
+			<h1 className='p-4 pb-0 text-center font-mabryProBold text-4xl text-slate-700'>
+				A demo for MarkWriter
+			</h1>
+
+			<p className='px-6 py-4 font-mono text-lg text-slate-500'>
+				# A demo of `react-markdown` :heart: :+1: :rocket:
+				<br />
+				<br />
+				`react-markdown` is a markdown component for React. 👉 Changes are
+				re-rendered as you type. 👈 Try writing some markdown on the left.
+			</p>
 		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	darkMode: true,
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				mono: ["'DM Mono'", ...defaultTheme.fontFamily.mono],
+				mabryProRegular: [
+					"'MabryPro-regular'",
+					...defaultTheme.fontFamily.sans
+				],
+				mabryProBold: ["'MabryPro-bold'", ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };
