929a06165b167166a0aee7459d416226661d99fe
nabeelcodes <nabeeldesigner91@hotmail.com>
2023-07-09 01:27:13 +0530

    Added multiple SVGs as React components
    - Added SVG based React components in `src/assets`.
    - Added css class : `container2000` to apply TW styles for a custom container of `max-width: 2000px`

---
+++ src/assets/HamburgerSVG.tsx
@@ -0,0 +1,30 @@
+export const HamburgerIcon = () => {
+	return (
+		<svg
+			width='36'
+			height='23'
+			viewBox='0 0 36 23'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M3 3H32.9997'
+				stroke='#4B4B4B'
+				strokeWidth='5'
+				strokeLinecap='round'
+			/>
+			<path
+				d='M19.9233 19.923L33.0001 19.923'
+				stroke='#4B4B4B'
+				strokeWidth='5'
+				strokeLinecap='round'
+			/>
+			<path
+				d='M11.4609 11.4615L32.9992 11.4615'
+				stroke='#4B4B4B'
+				strokeWidth='5'
+				strokeLinecap='round'
+			/>
+		</svg>
+	);
+};
