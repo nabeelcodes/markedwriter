07b1061ae4b0e84858cdbe31b32b29bb56d4abe6
nabeelcodes <nabeeldesigner91@hotmail.com>
2023-07-09 02:03:05 +0530

    Renamed app to `MarkedWriter`

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# MarkWriter
+# MarkedWriter
 
-A simple react markdown editor and previewer built with Unified(remark-parse) and TailwindCSS
+A simple react markdown editor and previewer built with Unified(remark-parse), Radix-UI and TailwindCSS.
--- src/utilities/tabHelper.ts
+++ src/utilities/tabHelper.ts
@@ -1,23 +1,25 @@
-export const insertTabForTextarea = (
-	event: React.KeyboardEvent<HTMLTextAreaElement>,
-	setContent: React.Dispatch<React.SetStateAction<string>>
-) => {
-	if (event.key == 'Tab') {
-		event.preventDefault();
+// export const insertTabForTextarea = (
+// 	event: React.KeyboardEvent<HTMLTextAreaElement>,
+// 	setContent: React.Dispatch<React.SetStateAction<string>>
+// ) => {
+// 	if (event.key == 'Tab') {
+// 		event.preventDefault();
 
-		const textArea = event.target;
-		const { selectionStart, selectionEnd } = textArea;
-		const newText =
-			textArea.value.substring(0, selectionStart) +
-			'  ' + // Edit this for tab space, currently 2
-			textArea.value.substring(selectionEnd, textArea.value.length);
+// 		const textArea = event.target;
+// 		const { selectionStart, selectionEnd } = textArea;
+// 		const newText =
+// 			textArea.value.substring(0, selectionStart) +
+// 			'  ' + // Edit this for tab space, currently 2
+// 			textArea.value.substring(selectionEnd, textArea.value.length);
 
-		textArea.focus();
+// 		textArea.focus();
 
-		textArea.value = newText;
+// 		textArea.value = newText;
 
-		textArea.setSelectionRange(selectionStart + 2, selectionStart + 2);
+// 		textArea.setSelectionRange(selectionStart + 2, selectionStart + 2);
 
-		setContent(newText);
-	}
-};
+// 		setContent(newText);
+// 	}
+// };
+
+export {};
